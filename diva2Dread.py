929f77b08152061fb4d3c6a80d953c7d6e6a2961
Charles Troupin <ctroupin@socib.es>
2017-01-19 15:41:36 +0100

    function to read parameters and plot grid

--- diva2Dread.py
+++ diva2Dread.py
@@ -36,16 +36,68 @@ def read_contour(ContourFile):
 	
 	return contours
 	
+def read_data(DataFile):
+	'''Read the information contained in a DIVA data file
+	lon, lat, value, (field)
+	'''
+	lon, lat, field = np.loadtxt(DataFile, unpack=True, usecols=(0, 1, 2))
+	#TODO
+	# Check performance for huge files
+	# Check for files with different number of columns (weight)
+
+	return lon, lat, field
+	
+def read_parameters(ParamFile):
+	'''Read the information contained in a DIVA parameter file
+	and extract the analysis parameters
+	'''
+	lc, icoord, ispec, ireg, xori, yori, dx, dy, nx,\
+	ny, valex, snr, varbak = np.loadtxt(ParamFile, 
+	                                    comments='#', unpack=True)
+
+	return lc, icoord, ispec, ireg, xori, yori,\
+	dx, dy, nx, ny, valex, snr, varbak
+	
 def plot_contour(contours):
 	for contour in contours:
 		# Append first element of the array to close the contour
-		plt.plot(contour[:, 0], contour[:, 1])
+		plt.plot(np.append(contour[:, 0], contour[0, 0]), 
+		         np.append(contour[:, 1], contour[0, 1]))
+		         
+def plot_datapoints(lon, lat, *args, **kwargs):
+	if len(args) == 1:
+		plt.scatter(lon, lat, c=args[0], **kwargs)
+		cbar = plt.colorbar()
+	else:
+		plt.scatter(lon, lat, **kwargs)
+		
+def plot_outputgrid(xori, yori, dx, dy, nx, ny, scalefactor=1, **kwargs):
+    '''Plot the specified output grid for the analyzed field.
+    If the grid is too dense, use a scale factor larger than 1.
+    The scale factor is rounded if not chosen as integer.
+    '''
+    if not(type(scalefactor) == 'int'):
+        scalefactor = round(scalefactor)
+
+    # derive xmax and ymax
+    xend, yend = xori + (nx + 1) * dx, yori + (ny + 1) * dy
+    xx, yy = np.arange(xori, xend, scalefactor * dx), np.arange(yori, yend, scalefactor * dy)
+    plt.hlines(yy, xori, xend, **kwargs, linewidth=0.2)
+    plt.vlines(xx, yori, yend, **kwargs, linewidth=0.2)
+		
 	
 if __name__ == '__main__':
 	contourfile = '/home/ctroupin/Software/DIVA/diva-4.7.1/DIVA3D/divastripped/input/coast.cont'
+	datafile = '/home/ctroupin/Software/DIVA/diva-4.7.1/DIVA3D/divastripped/input/data.dat'
+	ParamFile = '/home/ctroupin/Software/DIVA/diva-4.7.1/DIVA3D/divastripped/input/param.par'
+	
 	contours = read_contour(contourfile)
+	lon, lat, field = read_data(datafile)
+	lc, icoord, ispec, ireg, xori, yori, dx, dy, nx, ny,\
+	valex, snr, varbak = read_parameters(ParamFile)
 	
 	fig = plt.figure()
 	plot_contour(contours)
-	plt.savefig('./test_contour.png')
+	plot_datapoints(lon, lat, marker='o', color='k')
+	plt.savefig('./test_contour_data.png')
 	plt.close()
