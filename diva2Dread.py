be45e59c0ec5c604a7a294da4a561aa796d189f1
Charles Troupin <ctroupin@socib.es>
2017-02-06 17:17:58 +0100

    update reading tools

--- diva2Dread.py
+++ diva2Dread.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
-'''Functions to read the main Diva 2D input files
-'''
+"""Functions to read the main Diva 2D input files
+"""
 
 import os
 import linecache
@@ -12,12 +12,12 @@ import logging
 
 
 def read_contour(ContourFile):
-	'''Read the information contained in a DIVA contour file
-	'''
+	"""Read the information contained in a DIVA contour file
+	"""
 	contours = []
 	
-	# Count number of files and contours
-	ncontours = int(linecache.getline(ContourFile, 1))
+	# Count number of contours and lines
+        ncontours = int(linecache.getline(ContourFile, 1))
 	nlines = sum(1 for line in open(ContourFile))
 	
 	# Initialise line to read number
@@ -41,9 +41,9 @@ def read_contour(ContourFile):
 	return contours
 	
 def read_data(DataFile):
-	'''Read the information contained in a DIVA data file
+	"""Read the information contained in a DIVA data file
 	lon, lat, value, (field)
-	'''
+	"""
 	lon, lat, field = np.loadtxt(DataFile, unpack=True, usecols=(0, 1, 2))
 	#TODO
 	# Check performance for huge files
@@ -52,9 +52,9 @@ def read_data(DataFile):
 	return lon, lat, field
 	
 def read_parameters(ParamFile):
-	'''Read the information contained in a DIVA parameter file
+	"""Read the information contained in a DIVA parameter file
 	and extract the analysis parameters
-	'''
+	"""
 	lc, icoord, ispec, ireg, xori, yori, dx, dy, nx,\
 	ny, valex, snr, varbak = np.loadtxt(ParamFile, 
 	                                    comments='#', unpack=True)
@@ -63,8 +63,8 @@ def read_parameters(ParamFile):
 	dx, dy, nx, ny, valex, snr, varbak
 	
 def read_mesh(MeshFile, MeshTopoFile):
-    '''Read the coordinates of the finite element mesh.
-    '''
+    """Read the coordinates of the finite element mesh.
+    """
     # Load mesh caracteristics
     datamesh = np.loadtxt(MeshTopoFile)
     nnodes = int(datamesh[0])
@@ -94,10 +94,10 @@ def read_mesh(MeshFile, MeshTopoFile):
     return xnode, ynode, i1, i2, i3
     
 def read_results(ResultFile):
-    '''Read the analyzed field, the error field (if exists) and their coordinates
+    """Read the analyzed field, the error field (if exists) and their coordinates
     from the netCDF file. 
     If the error field doesn't exist, the function return a field full of NaN's.
-    '''
+    """
     with netCDF4.Dataset(ResultFile) as nc:
         x = nc.variables['x'][:]
         y = nc.variables['y'][:]
