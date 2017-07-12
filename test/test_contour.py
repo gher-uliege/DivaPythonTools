f7979078513262b4c068c6179c9c93b82a5e685a
ctroupin <charles.troupin@gmail.com>
2017-07-12 11:39:12 +0200

    chained methods

--- test/test_contour.py
+++ test/test_contour.py
@@ -6,12 +6,12 @@ import unittest
 class TestContourMethods(unittest.TestCase):
 
     def setUp(self):
-        self.xlist = [[0.7173, 10.9596, 10.53612105, 0.71213338], [3.424071, 23.9648, 23.8063, 3.4999]]
-        self.ylist = [[-1.7529, -1.257, 5.37686511, 5.00245219], [1.6594, 1.945, 3.7673, 4.36759776]]
+        self.xlist = [[0.7173, 10.9596, 0.71213338], [3.424071, 23.9648, 23.8063, 3.4999]]
+        self.ylist = [[-1.7529, -1.257, 5.00245219], [1.6594, 1.945, 3.7673, 4.36759776]]
         self.xx = np.array(((0.1, 4., 4, 1), (6.3, 9.3, 8.8, 7.4)))
         self.yy = np.array(((0.3, 1., 3.4, 1.8), (-1., -1.4, 2.2, 3.3)))
-        self.divadir = "/home/ctroupin/Software/DIVA/DIVA-diva-4.7.1/"
         self.coastfile = "../data/coast.cont"
+        self.nocoastfile = "../data/nocoast.cont"
 
     def test_init(self):
         """
@@ -26,17 +26,16 @@ class TestContourMethods(unittest.TestCase):
         """
         Instantiate Contour object with a list of lists
         """
-        contour = pydiva2d.Diva2DContours(self.xx, self.yy)
-        np.testing.assert_array_equal(contour.x, self.xx)
-        np.testing.assert_array_equal(contour.y, self.yy)
+        contour = pydiva2d.Diva2DContours(self.xlist, self.ylist)
+        np.testing.assert_array_equal(contour.x, self.xlist)
+        np.testing.assert_array_equal(contour.y, self.ylist)
         self.assertEqual(contour.get_contours_number, 2)
 
     def test_read_file(self):
         """
         Instantiate Contour object reading an existing file
         """
-        contour = pydiva2d.Diva2DContours()
-        contour.read_from(self.coastfile)
+        contour = pydiva2d.Diva2DContours().read_from(self.coastfile)
         self.assertEqual(contour.get_contours_number, 28)
         self.assertEqual(len(contour.x), 28)
         self.assertEqual(len(contour.y), 28)
@@ -49,14 +48,20 @@ class TestContourMethods(unittest.TestCase):
         Instantiate Contour object reading an existing file
         using numpy module
         """
-        contour = pydiva2d.Diva2DContours()
-        contour.read_from_np(self.coastfile)
+        contour = pydiva2d.Diva2DContours().read_from_np(self.coastfile)
         self.assertEqual(contour.get_contours_number, 28)
         self.assertEqual(len(contour.x), 28)
         self.assertEqual(len(contour.y), 28)
         self.assertEqual(len(contour.x[0]), 576)
         self.assertEqual(len(contour.y[1]), 16)
         self.assertEqual(len(contour.x[-1]), 6)
+
+    def test_read_nonexisting_file(self):
+        """
+        Try instantiate Contour object reading an non-existing file
+        """
+        self.assertRaises(FileNotFoundError,
+                          lambda: pydiva2d.Diva2DContours().read_from(self.nocoastfile))
 
 if __name__ == '__main__':
     unittest.main()
