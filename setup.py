1f0fcc56c9b40937fd6e47fe34fe44e15977dc95
ctroupin <charles.troupin@gmail.com>
2018-04-30 10:28:49 +0200

    added email

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         description='A set of Python tools to help users with: 1. The preparation of Diva input files: data, contours, parameters; 2. The execution of the Diva interpolation tool; 3. The reading of output files (analysis, finite-element mesh); 4. The input and output plotting.',
         long_description=readme,
         author='Charles Troupin',
-        author_email='',
+        author_email='charles.troupin@gmail.com',
         url='https://github.com/gher-ulg/DivaPythonTools',
         license=license,
         packages=find_packages(exclude=('test', 'Notebooks', 'data', 'figures'))
